93c6a5d1adf8ab8580e9f01b9eb3b40ad5ffd2ae
Danillo Ferreira <danillo.ferreira.pinto@gmail.com>
2022-01-19 19:24:39 -0300

    Conectando com JPA

--- src/main/java/com/example/cursomc/domain/Categoria.java
+++ src/main/java/com/example/cursomc/domain/Categoria.java
@@ -3,16 +3,26 @@ package com.example.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nome;
-	
+
 	public Categoria() {
-		
+
 	}
-	
+
 	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
@@ -51,7 +61,5 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
 	}
-	
-	
 
 }
